b5a1ff179cca12626d6aa8254d88d1ad9273a587
loanMaster <director@emailn.de>
2024-12-08 10:01:59 +0100

    fix: findCoingeckoToken

--- server/src/util/find-coingecko-token-id.ts
+++ server/src/util/find-coingecko-token-id.ts
@@ -1,24 +1,11 @@
-import * as coingeckoTokens from "../../res/coingecko-tokens.json";
+import coingeckoTokens from "../../res/coingecko-tokens.json";
 import {logger} from "../logger/logger";
-import * as substrateChains from "../../res/substrate-chains.json";
 
 export const findCoingeckoToken = (symbol: string, chain: string): { id: string, symbol: string } | undefined => {
-    const isSubtrateChain = substrateChains.chains.some(p => p.name === chain)
-    const platform = isSubtrateChain ? 'polkadot' : chain
+    // const isSubtrateChain = substrateChains.chains.some(p => p.name === chain)
+    //const platform = isSubtrateChain ? 'polkadot' : chain
 
-    let tokens = coingeckoTokens.tokens.filter(p => p.symbol.toLowerCase() === symbol.toLowerCase() && p.id.indexOf('-peg-') == -1)
-    if (tokens.length > 1) {
-        const tokensTemp = tokens.filter(t => platform && t.platforms?.[chain] !== undefined)
-        if (tokensTemp.length === 1) {
-            tokens = tokensTemp
-        }
-    }
-    if (tokens.length > 1) {
-        const tokensTemp = tokens.filter(t => t.platforms !== undefined && Object.keys(t.platforms).length === 0)
-        if (tokensTemp.length === 1) {
-            tokens = tokensTemp
-        }
-    }
+    let tokens = coingeckoTokens.filter(p => p.symbol.toLowerCase() === symbol.toLowerCase() && p.id.indexOf('-peg-') == -1)
     if (tokens.length === 0) {
         logger.warn("Coingecko token for symbol " + symbol + " not found", new Error().stack)
         return undefined
