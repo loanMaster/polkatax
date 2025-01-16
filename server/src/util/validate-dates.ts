d09bda61c8116b529993a04bbe3d8a9d819a22f2
loanMaster <director@emailn.de>
2025-01-16 08:53:55 +0100

    feat: Cannot select back to the start of the Acala blockchain. #4

--- server/src/util/validate-dates.ts
+++ server/src/util/validate-dates.ts
@@ -1,8 +1,6 @@
 import {HttpError} from "../error/HttpError";
 import {formatDate} from "./format-date";
 
-const TWO_YEARS = 1000 * 60 * 60 * 24 * 365 * 2
-
 export const validateDates = (startDate: Date, endDate?: Date) => {
     if (startDate > (endDate || Date.now())) {
         throw new HttpError(400, "Start date after end date")
