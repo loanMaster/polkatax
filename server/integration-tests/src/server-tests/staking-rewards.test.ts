3cfce88c0deb560ec0bcf0d38615a707d6abde15
loanMaster <nicolai.fricker@gmx.ch>
2025-05-11 21:42:51 +0200

    fix: integration test ci

--- server/integration-tests/src/server-tests/staking-rewards.test.ts
+++ server/integration-tests/src/server-tests/staking-rewards.test.ts
@@ -29,7 +29,7 @@ describe("should fetch staking rewards", () => {
     fastiyInstance = await polkataxServer.init(3001);
 
     const response = await fetch(
-      "http://localhost:3001/api/staking-rewards/kusama/5GeJMTfNpe2mmJgnxHoYJDVvNFcn8X4fbdtVPHVonFSX9tH7?startdate=1704063600000&enddate=1735686000000&currency=USD",
+      "http://127.0.0.1:3001/api/staking-rewards/kusama/5GeJMTfNpe2mmJgnxHoYJDVvNFcn8X4fbdtVPHVonFSX9tH7?startdate=1704063600000&enddate=1735686000000&currency=USD",
     );
     const jsonBody = await response.json();
     verifyEquals(jsonBody, "staking_rewards_ksm_2024.json");
