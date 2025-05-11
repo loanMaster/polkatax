3b77e0e927cd2c07ab22e42f8536fa99412f1df7
loanMaster <nicolai.fricker@gmx.ch>
2025-05-11 22:05:21 +0200

    fix: integration test ci

--- server/integration-tests/src/e2e-tests/ksm-staking-rewards.test.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("homepage has title", async ({ page }) => {
-  await page.goto("/staking-rewards");
-  await expect(page).toHaveTitle(/PolkaTax/);
-});
